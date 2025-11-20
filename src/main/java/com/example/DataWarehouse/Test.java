cd328567021f7957aa7fbcc9c3d145efe7e5e66a
tienhoangg <tienhoangle31052005@gmail.com>
2025-11-21 01:15:24 +0700

    chuyen com.example.Warehouse/Test.java sang src/main/java

---
+++ src/main/java/com/example/DataWarehouse/Test.java
@@ -0,0 +1,40 @@
+package com.example.DataWarehouse;
+
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+import config.Config;
+import java.io.File;
+import java.util.List;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import process.extract.Scraper;
+import process.extract.WeatherData;
+import process.loadwh.LoadWarehouseProcess;
+
+
+public class Test {
+
+	public static void main(String[] args) throws Exception {
+		XmlMapper xmlMapper = new XmlMapper();
+		Config config = xmlMapper.readValue(new File("D:/DW/DataWarehouse/config.xml"), Config.class);
+
+		System.out.println("Host: " + config.database.host);
+		System.out.println("Port: " + config.database.port);
+		System.out.println("Src Folder Path: " + config.source.source_folder_path);
+        System.out.println("Src Transaction Path: " + config.transaction.scripts);
+        System.out.println("Src Aggregate Path: " + config.aggregate.scripts);
+        System.out.println("Src Aggregate Path: " + config.mart.scripts);
+
+        String url = config.source.source_url;
+        Document doc = Jsoup.connect(url).get();
+        WeatherData data = Scraper.fetchWeatherData(doc);
+		String fileName = Scraper.generateFileName(config.source.source_folder_path);
+		Scraper.writeToCSV(data, fileName);
+
+		// LOAD WAREHOUSE
+		LoadWarehouseProcess loadWH = new  LoadWarehouseProcess();
+		loadWH.runLoadWarehouse(List.of("D:\\DW\\scripts\\proc_load_warehouse.sql"));
+	}
+
+}
