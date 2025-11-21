a70e7ea859f0279f83dc4586c117f2f558ddb95f
BaoTam368 <23130286@st.hcmuaf.edu.vn>
2025-11-21 22:14:23 +0700

    Update control for extract + personal work

--- src/test/java/com/example/DataWarehouse/Test.java
+++ src/test/java/com/example/DataWarehouse/Test.java
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
 import config.Config;
+import config.MainConfig;
 import database.Control;
 import database.DataBase;
 
@@ -19,16 +20,17 @@ import extract.*;
 public class Test {
 
 	public static void main(String[] args) throws Exception {
-		XmlMapper xmlMapper = new XmlMapper();
-		Config config = xmlMapper.readValue(new File("config.xml"), Config.class);
+
+		Config config = MainConfig.readConfig();
 
 		Connection conn = DataBase.connectDB(config.database.host, config.database.port, config.database.user,
 				config.database.password, "control");
 
 		int source_id = Control.insertConfigSource(conn, config.source.source_name, config.source.source_url,
-				config.source.source_folder_path, "csv", config.extract.scraping_script_path, "?", "?", "?", "?", "?", "?");
+				config.source.source_folder_path, "csv", config.extract.scraping_script_path, "?", "?", "?", "?", "?",
+				"?");
 
-		Main.main(args, conn, config, source_id);
+		MainExtract.main(args, conn, config, source_id);
 
 	}
 
