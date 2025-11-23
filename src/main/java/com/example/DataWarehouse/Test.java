04f5f679af00bb21c6c962ea111524d7d61a1127
tienhoangg <tienhoangle31052005@gmail.com>
2025-11-23 18:18:43 +0700

    sucessfully test and run proc_load_warehouse.sql

--- src/main/java/com/example/DataWarehouse/Test.java
+++ src/main/java/com/example/DataWarehouse/Test.java
@@ -10,7 +10,9 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import process.extract.Scraper;
 import process.extract.WeatherData;
+import process.loadcsv.loadCsvToStaging;
 import process.loadwh.LoadWarehouseProcess;
+import process.transform.TransformProcess;
 
 
 public class Test {
@@ -26,15 +28,22 @@ public class Test {
         System.out.println("Src Aggregate Path: " + config.aggregate.scripts);
         System.out.println("Src Aggregate Path: " + config.mart.scripts);
 
+		//EXTRACT
         String url = config.source.source_url;
         Document doc = Jsoup.connect(url).get();
+
         WeatherData data = Scraper.fetchWeatherData(doc);
 		String fileName = Scraper.generateFileName(config.source.source_folder_path);
 		Scraper.writeToCSV(data, fileName);
+		//LOAD CSV
+		loadCsvToStaging.load(fileName);
+		//TRANSFORM
+		TransformProcess transform = new TransformProcess();
+		transform.runTransform(config.transaction.scripts);
 
 		// LOAD WAREHOUSE
 		LoadWarehouseProcess loadWH = new  LoadWarehouseProcess();
-		loadWH.runLoadWarehouse(List.of("D:\\DW\\scripts\\proc_load_warehouse.sql"));
+		loadWH.runLoadWarehouse(List.of("D:\\DW\\DataWarehouse\\database\\warehouse\\proc_load_warehouse.sql"));
 	}
 
 }
