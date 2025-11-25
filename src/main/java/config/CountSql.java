14b2b4178baf3805b4e3a8171298191e735af77e
TienHoang <tienhoangle31052005@gmail.com>
2025-11-26 01:47:56 +0700

    Delete src/main/java/config/CountSql.java
    delete

--- src/main/java/config/CountSql.java
+++
@@ -1,8 +0,0 @@
-package config;
-
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
-
-public class CountSql {
-    @JacksonXmlProperty(localName = "path")
-    public String path;
-}
